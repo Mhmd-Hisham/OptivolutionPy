3fbd597ed2979c799d1508884c62fab944489099
Mohamed Hisham <Mohamed00Hisham@Gmail.com>
2019-08-19 01:23:12 +0200

    Update one_dimensional_knapsack.py

--- examples/one_dimensional_knapsack.py
+++ examples/one_dimensional_knapsack.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-    Example project.
-    One dimensional knapsack.
+    OptivolutionPy example project.
+    Solving the One dimensional knapsack problem.
 """
 
 import random
 
-from GA.Population import Population
-from GA.Chromosome import Chromosome
+from optivolution.population import Population
+from optivolution.chromosome import Chromosome
 
 class OneDimensinalKnapsack(Chromosome):
     """ Inidividual knapsack object. """
